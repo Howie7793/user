ed56b61f95ce7b2b5f5a30dd556a4d299bb2d178
howie <howie-shihuai@foxmail.com>
2022-08-16 14:26:23 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
+    <h1>我是测试</h1>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="less">
